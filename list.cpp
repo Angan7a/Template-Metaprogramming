556f06c4b445369fb121dd6f21b4d718f51f7795
Andrzej <angan@o2.pl>
2017-04-21 11:13:51 +0200

    Works methode find N in list

--- list.cpp
+++ list.cpp
@@ -2,11 +2,11 @@
 
 
 struct NIL {
-    typedef NIL Head;
-    typedef NIL Tail;
+//    typedef NIL Head;
+//    typedef NIL Tail;
 };
 
-template <typename H=NIL, typename T=NIL>
+template <typename H, typename T=NIL>
 struct Lst {
     typedef H Head;
     typedef T Tail;
@@ -29,13 +29,31 @@ struct How_long<NIL> {
     static const int result = 0;
 };
 
+template<bool a, typename Lista, int N>
+struct CzyZawiera {
+    static const bool out = CzyZawiera<(Lista::Head::result == N), typename Lista::Tail, N>::out;
+};
 
+template< int N>
+struct CzyZawiera<0,NIL,N> {
+    static const bool out = 0;
+};
+
+template<typename Lista, int N>
+struct CzyZawiera<1,Lista,N> {
+   static const bool out = 1;
+};
+
+
+template<typename Lista, int N>
+struct Find {
+    static const bool output = CzyZawiera<(Lista::Head::result == N), Lista, N>::out;
+};
 
 int main()
 {
 //    typedef Lst< Int<1>, Lst< Int<2>, Lst< Int<3> > > > OneTwoThree;
 //     std::cout << Int<3>::result << std::endl;
-     typedef Lst<> f;
 //     std::cout << f1::Head::result << std::endl;
 
      typedef Lst< Int<3> > f1;
@@ -47,9 +65,13 @@ int main()
      typedef Lst< Int<1>, f2 > f3;
      std::cout << f3::Head::result << std::endl;
 
-  typedef Lst< Int<1>, Lst< Int<2>, Lst< Int<3> > > > OneTwoThree;
+     typedef Lst< Int<1>, Lst< Int<2>, Lst< Int<3> > > > OneTwoThree;
      std::cout << OneTwoThree::Tail::Tail::Head::result << std::endl;
 
-     std::cout << How_long<f2>::result << std::endl;
+     std::cout << "List length: " << How_long<f3>::result << std::endl;
+     std::cout << "Czy lista zawiera 4: " << Find<f3,4>::output << std::endl;
+
+
+
 return 0;
 }
