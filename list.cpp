539e26df41799b2d6dc391e773311626a70e2c9d
Andrzej <angan@o2.pl>
2017-04-21 16:08:12 +0200

    Works Append, Prepend, count positin of found N (but it need imporve when N is not in list)

--- list.cpp
+++ list.cpp
@@ -32,22 +32,46 @@ struct How_long<NIL> {
 template<bool a, typename Lista, int N>
 struct CzyZawiera {
     static const bool out = CzyZawiera<(Lista::Head::result == N), typename Lista::Tail, N>::out;
+    static const int na_jakiej_pozycji = 1 + CzyZawiera<(Lista::Head::result == N), typename Lista::Tail, N>::na_jakiej_pozycji;
 };
 
 template< int N>
 struct CzyZawiera<0,NIL,N> {
     static const bool out = 0;
+    static const int na_jakiej_pozycji = 1;
 };
 
 template<typename Lista, int N>
 struct CzyZawiera<1,Lista,N> {
    static const bool out = 1;
+   static const int na_jakiej_pozycji = 1;
 };
 
 
 template<typename Lista, int N>
 struct Find {
-    static const bool output = CzyZawiera<(Lista::Head::result == N), Lista, N>::out;
+//    static const bool output = CzyZawiera<(Lista::Head::result == N), Lista, N>::out;
+    typedef CzyZawiera<(Lista::Head::result == N), Lista, N> a;
+    static const bool output = a::out;
+    static const int p = a::na_jakiej_pozycji;
+};
+
+template<typename Element, typename Lista=NIL>
+struct DopiszPo {
+    typedef Lst<Element, Lista> result;
+};
+
+template<typename Element, typename Lista>
+struct DopiszPrzed {
+   typedef typename Lista::Head Head;
+   typedef typename Lista::Tail Tail;
+   typedef typename DopiszPrzed<Element, Tail>::out Nastepny;
+   typedef Lst<Head, Nastepny> out;
+};
+
+template<typename Element>
+struct DopiszPrzed<Element, NIL> {
+    typedef Lst<Element> out;
 };
 
 int main()
@@ -68,8 +92,11 @@ int main()
      typedef Lst< Int<1>, Lst< Int<2>, Lst< Int<3> > > > OneTwoThree;
      std::cout << OneTwoThree::Tail::Tail::Head::result << std::endl;
 
-     std::cout << "List length: " << How_long<f3>::result << std::endl;
-     std::cout << "Czy lista zawiera 4: " << Find<f3,4>::output << std::endl;
+     typedef DopiszPrzed< Int<4>, f3 >::out d;
+     typedef DopiszPo< Int<124>, d >::result d1;
+     std::cout << "List length: " << How_long<d1>::result << std::endl;
+     std::cout << "Czy lista zawiera 3: " << Find<d1,3>::output << std::endl;
+     std::cout << "Na pozycji: " << Find<d1,3>::p << std::endl;
 
 
 
